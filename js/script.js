deb98972cc20e1025d61c436cd52f12d60952411
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-03-19 12:33:28 +0000

    Add createGrid() function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 main();
 
 function main() {
-  console.log('hello');
+  console.log(createGrid());
+}
+
+function createGrid(rowSize = 16) {
+  const container = document.querySelector('#container');
+  gridSize = rowSize ** 2;
+  for (let i = 0; i < gridSize; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+  }
+  return gridSize;
 }
